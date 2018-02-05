4f3c55ff35047c39a813a8abf19d4bb333055cce
aquilestg <aquiles.toruno2015@gmail.com>
2018-02-04 19:20:08 -0600

    Se pinta el nombre del usuario en popup de extension

--- content.js
+++ content.js
@@ -3,6 +3,6 @@ var logged_in_user
 chrome
     .extension
     .sendMessage({}, function (response) {
-        logged_in_user = response.email
-        console.log("Got user:", logged_in_user);
+        debugger;
+        logged_in_user = response.email;
     });
