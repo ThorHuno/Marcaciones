3647f1c3fdb138cb00db721d354b3b9b02604b56
aquilestg <aquiles.toruno2015@gmail.com>
2018-02-04 18:29:23 -0600

    Obteniendo el usuario de Chrome

---
+++ background.js
@@ -0,0 +1,12 @@
+chrome
+    .identity
+    .getProfileUserInfo(function (info) {
+        email = info.email;
+    });
+
+chrome
+    .extension
+    .onMessage
+    .addListener(function (request, sender, sendResponse) {
+        sendResponse({email: email})
+    });
---
+++ content.js
@@ -0,0 +1,8 @@
+var logged_in_user
+
+chrome
+    .extension
+    .sendMessage({}, function (response) {
+        logged_in_user = response.email
+        console.log("Got user:", logged_in_user);
+    });
