775d048e9f611f15a4902f6a34f6dff2e40dc78d
aquilestg <aquiles.toruno2015@gmail.com>
2018-02-06 22:35:23 -0600

    Axios

--- index.js
+++ index.js
@@ -9,6 +9,24 @@ document
 
                 if (nodoUsuario) {
                     nodoUsuario.innerText = `${logged_in_user}`;
+
+                    axios
+                        .get('https://jsonplaceholder.typicode.com/posts')
+                        .then(function (response) {
+                            localStorage.setItem('comentarios', JSON.stringify(response.data));
+                        })
+                        .catch(function (error) {
+                            console.log(error);
+                        });
                 }
             });
+
+        var btn = document.getElementById('btnMarcar');
+        btn.addEventListener('click', function () {
+            chrome
+                .tabs
+                .executeScript(null, {code: "document.body.style.backgroundColor= 'red'"});
+
+            // window.close();
+        });
     });
